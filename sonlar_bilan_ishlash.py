30065cd420f1317de3c4b01b0b9110f3e5613ffd
doniyorjonjaloliddinov <don2003.01.09@gmail.com>
2022-03-30 14:30:11 +0500

    initial commit

---
+++ sonlar_bilan_ishlash.py
@@ -0,0 +1,8 @@
+#Foydalanuvchidan son qabul qilib uning kvadratini hamda kubini hisoblovchi dastur
+print("Foydalanuvchidan son qabul qilib uning kvadratini hamda kubini hisoblovchi dastur")
+user_num = int(input("Istalgan son kiriting\n>>> "))
+kvadrat = user_num**2 
+kub = user_num**3
+print(f"{user_num} kvadrati {kvadrat} ga teng\n{user_num} ning kubi {kub}ga teng")
+
+
